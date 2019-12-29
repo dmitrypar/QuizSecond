c0df9a422a57430b7369c781aef9aa85b6497893
dmitriyPC <dmitrypn81@gmail.com>
2019-12-29 18:16:49 +0300

    corrected error

--- src/components/FinishPage.js
+++ src/components/FinishPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {qData} from "../service/qData";
 
-const Finish = (props) =>{
-    const  {score} = props.score;
+
+const Finish = ({qDataLength, score}) =>{
+
     return (
-        <div>
-            <h2>Поздравляю! Викторина окончена!</h2>
+        <div className="correctIs">
+            <h2>Поздравляем! Викторина окончена!</h2>
             <h2>
-                <span className="correctIs"> {`Правильных ответов ${score} из ${qData.length}`}</span>
+                <span > {`Правильных ответов ${score} из ${qDataLength}`}</span>
             </h2>
         </div>
     )
